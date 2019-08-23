f1969cda75f326b46694c50adff4f870a9e6760f
mario <jmferna91@gmail.com>
2019-08-23 15:18:15 -0400

    added a check to see if there is a title entered when creating a new article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -11,13 +11,19 @@ class ArticlesController < ApplicationController
 	end
 
 	def new
+		@article = Article.new
 	end
 
 	def create
 		@article = Article.new(article_params)
 
-		@article.save
-		redirect_to @article
+		#if at least 5 chars in article
+		if @article.save
+			redirect_to @article
+		else #otherwise tell user to make new article
+			#render will reuse the same request as before, if we had used redirect_to then the browser would issue another request
+			render 'new'
+		end
 	end
 
 	private
--- app/models/article.rb
+++ app/models/article.rb
@@ -1,2 +1,6 @@
 class Article < ApplicationRecord
+	#helps to validate the data we sent to models
+	#this will ensure that articles have a title of at least 5 chars
+  validates :title, presence: true,
+                    length: { minimum: 5 }
 end
