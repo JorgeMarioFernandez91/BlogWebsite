79376f7d1e59fc70a198bf7c9e0e63f2d184110d
mario <jmferna91@gmail.com>
2019-08-26 12:08:29 -0400

    added delete/destroy article functionality

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,6 +2,7 @@
 #that will become the actions for this controller
 #these actions are the CRUD operations
 class ArticlesController < ApplicationController
+
 	def index
 		@articles = Article.all
 	end
@@ -40,8 +41,17 @@ class ArticlesController < ApplicationController
 		end
 	end
 
+	def destroy
+		@article = Article.find(params[:id])
+		@article.destroy
+
+		redirect_to articles_path
+	end
+
 	private
+
 		def article_params
 			params.require(:article).permit(:title, :text)
 		end
+
 end
